57bd42db74e7999cd3950be8651768374a0f3668
Andelko Cvjetkovic <and3lko@gmail.com>
2022-04-12 19:32:18 +0200

    adding jest and setup base path to @app

---
+++ system.js
@@ -0,0 +1,6 @@
+/* globals System */
+System.config({
+  paths: {
+    '@app/*': './src/*'
+  }
+})
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  json: {
+    stringify: true,
+  },
+  resolve: {
+    alias: {
+      '@app': path.resolve(__dirname, './src')
+    },
+  },
+  base: '/',
+  server: {
+    port: 3000
+  },
+  preview: {
+    port: 3000
+  },
+  build: {
+    chunkSizeWarningLimit: 600,
+    cssCodeSplit: false,
+    rollupOptions: {
+      output: {
+        entryFileNames: '[hash].js',
+        assetFileNames: 'assets/[hash][extname]',
+        chunkFileNames: 'assets/[hash].js'
+      }
+    }
+  }
 });
