a74b9c35f4e529a856b22f3138794fcbef4ee896
Andelko Cvjetkovic <and3lko@gmail.com>
2022-04-15 17:13:18 +0200

    adding workshop header and display number

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,22 @@
-import Grid from '@mui/material/Grid';
-import FilterCategoryMobile from '@app/components/filter-category/mobile/FilterCategoryMobile';
-import FilterCategoryDesktop from '@app/components/filter-category/desktop/FilterCategoryDesktop';
+import Grid from "@mui/material/Grid";
+import FilterCategoryMobile from "@app/components/filter-category/mobile/FilterCategoryMobile";
+import FilterCategoryDesktop from "@app/components/filter-category/desktop/FilterCategoryDesktop";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
 function Home() {
   return (
-    <Grid container sx={{ mt: { xs: 2, lg: 7 } }}>
+    <Grid container sx={{ mt: { xs: 2, lg: 7 } }} rowSpacing={{ xs: 2 }}>
       <Grid item xs={12} md={3} xl={2}>
         <FilterCategoryMobile />
         <FilterCategoryDesktop />
       </Grid>
       <Grid item xs={12} md={9} xl={10}>
-        Sec
+        <Box pl={3}>
+          <Typography variant="h4" fontWeight={700} component="div">Workshops</Typography>
+          <Typography variant="subtitle1" fontWeight={700} fontSize={{ xs: 14, md: 16 }} color="grey.light">Displayed:
+            13</Typography>
+        </Box>
       </Grid>
     </Grid>
   );
